71dd345e66a6abf4baff9fce3a0fc34585be2fe1
shimizuyuta213@gmail.com <shimizuyuta213@gmail.com>
2024-01-02 19:22:03 +0900

    styled-componentsの設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea, {
+    margin: 0;
+    padding: 0;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    cursor: pointer;
+    text-decoration: none;
+    transition: all 0.3s ease-in-out;
+    color: #000;
+  }
+  ou ul {
+    list-style: none;
+  }
+`
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
