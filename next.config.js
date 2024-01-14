4a6f72b7fbce8fb2dd4403015446783e36592222
shimizuyuta213@gmail.com <shimizuyuta213@gmail.com>
2024-01-14 19:14:21 +0900

    next.configセットアップ

--- next.config.js
+++ next.config.js
@@ -2,9 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  }
+
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents: true,
+    }
+    //テストライブラリは後で入れる
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        reactRemovePropTypes: {
+          // 本番環境でreact testing libraryで使用するdata-testidを削除
+          properties: ['~data-testid$'],
+        },
+      }
+    }
+    return compilerConfig
+  })(),
 }
 
 module.exports = nextConfig
